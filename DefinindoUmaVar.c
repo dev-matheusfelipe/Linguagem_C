9fb11d4a16812a70dd0476e87ce477648bf04197
Matheus Felipe <64490699+MatheusFellype2016@users.noreply.github.com>
2021-10-19 23:13:30 -0300

    Update and rename PrimeiroCode.c to DefinindoUmaVar.c

---
+++ DefinindoUmaVar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    //Definindo uma variável 
+    int a = 5;
+    
+    //Imprimindo a variável "a"
+    printf("%d", a);
+    
+    //Concatenando
+    printf("\nO valor da var a e: %d", a);
+    
+    //Mudando o valor de "a"
+    a = 15;
+
+    //Lendo valores
+    scanf("%d", &a);
+    
+    //Imprimindo
+    printf("\nUhuuu");
+    
+    //Pausando
+    system("pause");
+}
