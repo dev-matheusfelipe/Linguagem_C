eacd1f642d165dcb75068e0a9abedcaeb49b8926
Matheus Felipe <64490699+MatheusFellype2016@users.noreply.github.com>
2021-10-19 22:32:33 -0300

    Add files via upload

---
+++ Primeiro_codigo.c
@@ -0,0 +1,11 @@
+// #include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("\n Qual é o seu nome? ");
+    char nome[20];
+    fflush(stdin);
+    scanf("%s", nome);
+    printf("Olá, %s! Tudo bem com você? \n", nome);
+    return 0;
+}
