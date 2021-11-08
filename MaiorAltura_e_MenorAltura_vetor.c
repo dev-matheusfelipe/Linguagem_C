32b42995a475e08b4e832d77d5d283ac28526aeb
Matheus Fellype <64490699+MatheusFellype2016@users.noreply.github.com>
2021-11-08 20:33:56 -0300

    Update MaiorAltura_e_MenorAltura_vetor.c

--- MaiorAltura_e_MenorAltura_vetor.c
+++ MaiorAltura_e_MenorAltura_vetor.c
@@ -3,25 +3,27 @@
 int main()
 {
    int i;
-   float altura[15];
+   float altura[5];
    float maior = 0;
    float menor = 1000;
    
-   for(i=0; i<15; i++) {
+   for(i=0; i<5; i++) {
        
        printf("Digite a altura da pessoa %i: ",i+1);
        scanf("%f",&altura[i]);
-       
        
        if (altura[i] > maior) {
            maior = altura[i];
        }
        
-       
        if (altura[i] < menor) {
            menor = altura[i];
        }
        
+   }
+   
+   for(i=0; i<5; i++) {
+       printf("\nA %iº altura digitada é: %0.2f", i+1, altura[i]);
    }
    
    printf("\nA maior altura é: %0.2f", maior);
