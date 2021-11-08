e54a5044d4e7cbd6c9cd77dcbefff2b7a5ef5244
Matheus Fellype <64490699+MatheusFellype2016@users.noreply.github.com>
2021-11-08 19:48:13 -0300

    Create MaiorAlturaEMenorAlturaVetor.c

---
+++ MaiorAlturaEMenorAlturaVetor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+   int i;
+   float altura[5];
+   float maior = 0;
+   float menor = 1000;
+   
+   for(i=0; i<5; i++) {
+       
+       printf("Digite a altura da pessoa %i: ",i+1);
+       scanf("%f",&altura[i]);
+       
+       
+       if (altura[i] > maior) {
+           maior = altura[i];
+       }
+       
+       
+       if (altura[i] < menor) {
+           menor = altura[i];
+       }
+       
+   }
+   
+   printf("\nA maior altura é: %0.2f", maior);
+   printf("\nA menor altura é: %0.2f", menor);
+   
+   return 0;
+}
