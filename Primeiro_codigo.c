9d5adaeac6b13df81ff11455ea4c5fe9cbc71689
Matheus Fellype <64490699+MatheusFellype2016@users.noreply.github.com>
2021-11-08 21:05:08 -0300

    Update Primeiro_codigo.c

--- Primeiro_codigo.c
+++ Primeiro_codigo.c
@@ -4,7 +4,6 @@ int main(int argc, char **argv)
 {
     printf("\n Qual é o seu nome? ");
     char nome[20];
-    fflush(stdin);
     scanf("%s", nome);
     printf("Olá, %s! Tudo bem com você? \n", nome);
     return 0;
