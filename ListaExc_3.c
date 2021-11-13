d92ab37d5f0260a1194684ae00832d4819c7f192
Matheus Felipe <64490699+MatheusFellype2016@users.noreply.github.com>
2021-11-13 19:38:38 -0300

    Update ListaExc_3.c

--- ListaExc_3.c
+++ ListaExc_3.c
@@ -183,7 +183,9 @@ média maior ou igual a 7.0. */
 // EXERCÍCIO  7
 
 // Pergunta exercício 7
- 
+/* Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma,
+a multiplicação e os números. */
+
 // Resposta exercício 7
 
 //===========================================================================================================================================//
@@ -191,7 +193,10 @@ média maior ou igual a 7.0. */
 // EXERCÍCIO  8
 
 // Pergunta exercício 8
- 
+/* Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada
+informação no seu respectivo vetor. Imprima a idade e a altura na ordem inversa
+a ordem lida.  */
+
 // Resposta exercício 8
 
 //===========================================================================================================================================//
@@ -199,6 +204,8 @@ média maior ou igual a 7.0. */
 // EXERCÍCIO  9
 
 // Pergunta exercício 9
+/* Faça um Programa que leia um vetor A com 10 números inteiros, calcule e
+mostre a soma dos quadrados dos elementos do vetor. */
  
 // Resposta exercício 9
 
@@ -207,7 +214,10 @@ média maior ou igual a 7.0. */
 // EXERCÍCIO  10
 
 // Pergunta exercício 10
- 
+/* Faça um Programa que leia dois vetores com 10 elementos cada. Gere um
+terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos
+elementos intercalados dos dois outros vetores. */ 
+
 // Resposta exercício 10
 
 //===========================================================================================================================================//
@@ -215,7 +225,8 @@ média maior ou igual a 7.0. */
 // EXERCÍCIO  11
 
 // Pergunta exercício 11
- 
+/* . Altere o programa anterior, intercalando 3 vetores de 10 elementos cada. */ 
+
 // Resposta exercício 11
 
 //===========================================================================================================================================//
@@ -223,7 +234,10 @@ média maior ou igual a 7.0. */
 // EXERCÍCIO  12
 
 // Pergunta exercício 12
- 
+/* Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que
+determine quantos alunos com mais de 13 anos possuem altura inferior à média
+de altura desses alunos. */ 
+
 // Resposta exercício 12
 
 //===========================================================================================================================================//
